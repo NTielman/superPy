51a8b26f8719c1fd32ce386d5e6afffac3d02a7a
Nneka Tielman <khalie_nne@yahoo.com>
2021-01-10 22:04:20 +0100

    added venv and main

---
+++ main.py
@@ -0,0 +1,27 @@
+# Imports
+import argparse
+import csv
+from datetime import date
+
+# Do not change these lines.
+__winc_id__ = 'a2bc36ea784242e4989deb157d527ba0'
+__human_name__ = 'superpy'
+
+
+#think of any cool additional features?
+'''(bestsellers ki products are selling the most right now)
+most sales happen on which day (consider holding a sale on these days
+alert for products 1 week away di kaduka prompt owner to put them on sale'''
+#setup virtualenv
+#write out all functions that return (print) desired results
+#import csv test functions writing to csv reports ipv print()
+#import date time (test manipulating and using datetime ipv hardcoded daytime
+#import matplotlib, test ploting function to plot out amount of sales, revenue etc.
+#import argparse, convert the stdin commands to call the juiste functions
+#import learn to work with rich for styling
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
